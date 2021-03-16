b9a43e1e5d9fd060da850994028c04d4b85de326
paroonk <paroonk@hotmail.com>
2021-03-16 11:30:20 +0700

    update django 3.1.7

--- Procfile
+++ Procfile
@@ -1,5 +1,5 @@
 release: python manage.py migrate
 
-web: gunicorn djheroku.wsgi --log-file -
+web: gunicorn config.wsgi --log-file -
 
 worker: python manage.py rqworker default
--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
-"""djheroku URL Configuration
+"""heroku URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('keygen.urls')),
+    path('', include('webapp.urls')),
     path('admin/', admin.site.urls),
     path('django-rq/', include('django_rq.urls')),
 ]
